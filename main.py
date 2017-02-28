53e30a100b5f1ebfa7c0bd5260da416a85f3d021
jedi <ragpanda@163.com>
2017-02-28 20:46:29 +0800

    过滤广告

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 #-*- coding:utf-8 -*-
+from importlib import reload
+
 from lib.Master import Master
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 m = Master("./config/main.json")
 m.run_proxy()
